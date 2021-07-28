f6e91301a9cc7fef038f756bbbb0cc679a71b57d
saidskander <askander199h@outlook.com>
2021-07-29 01:30:03 +0200

    Done

--- 0x04-pagination/1-simple_pagination.py
+++ 0x04-pagination/1-simple_pagination.py
@@ -1,41 +1,48 @@
 #!/usr/bin/env python3
-""" Simple pagination """
+
+"""
+1. Simple pagination
+"""
 import csv
 import math
-from typing import List, Tuple
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Function should return a tuple of size two containing a start
+    index and an end index corresponding to the range of indexes to
+    return in a list for those particular pagination parameters
+    """
+    return ((page - 1) * page_size, page * page_size)
 
 
 class Server:
-    """paginate a database"""
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """ init
+        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """ dataset """
+        """dataset cache
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
+
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ List of the pagination done """
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-        range:
-            Tuple = index_range(page, page_size)
-        pagination:
-            List = self.dataset()
-        return (pagination[range[0]:range[1]])
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ tuple with the range start then size page """
-    end_size:
-        int = page * page_size
-    start_size:
-        int = end_size - page_size
-    return (start_size, end_size)
+        """ dataset
+        """
+        for param in [page, page_size]:
+            assert isinstance(param, int) and page > 0
+        self.dataset()
+        range_pagination = index_range(page=page, page_size=page_size)
+        return self.__dataset[range_pagination[0]:range_pagination[1]]
