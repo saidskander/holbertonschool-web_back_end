8dc854a786ac39ee16f1191b9edd8c83995b9e6d
saidskander <askander199h@outlook.com>
2021-08-22 22:07:04 +0200

    Done

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -58,5 +58,16 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def password_token():
+    """ get/generate a new password token, 200 respond status """
+    try:
+        email = request.form.get('email')
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
