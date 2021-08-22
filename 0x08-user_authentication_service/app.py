54a3f5e750c985e49b83d8d34465577567cfba9a
saidskander <askander199h@outlook.com>
2021-08-22 22:01:49 +0200

    Done

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -37,5 +37,16 @@ def session_login():
     return res
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ Log out """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if session_id is None or user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
