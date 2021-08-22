b4ec40562dd25276ea26af4cbe899b0fb070e772
saidskander <askander199h@outlook.com>
2021-08-22 21:59:50 +0200

    Done

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -7,16 +7,9 @@ AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def welcome():
+def Welcom():
     """ start a flask app """
     return jsonify({"message": "Bienvenue"})
-    email = request.form.get('email')
-    password = request.form.get('password')
-    try:
-        AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"}), 200
-    except Exception:
-        return jsonify({"message": "email already registered"}), 400
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
@@ -29,6 +22,19 @@ def users():
         return jsonify({"email": email, "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def session_login():
+    """ Session log-in """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password) is False:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    res = jsonify({'email': email, 'message': 'logged in'})
+    res.set_cookie('session_id', session_id)
+    return res
 
 
 if __name__ == "__main__":
