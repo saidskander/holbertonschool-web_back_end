2c0badff69c3c9013a125d14ebfe4bf9affc921b
saidskander <askander199h@outlook.com>
2021-08-27 00:36:06 +0200

    Done

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Flask app"""
+from flask import render_template
+from flask import Flask
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+""" App Flask """
+babel = Babel(app)
+""" Babel, TIME zone/Locale """
+
+
+class Config(object):
+    """ class configuration """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+""" make this class as config default for Flask application """
+
+
+@app.route("/")
+def index():
+    """Flask app"""
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ best match with our supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
