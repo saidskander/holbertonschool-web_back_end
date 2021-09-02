092dc773a23f487d7c793694c9db564fc2d36347
saidskander <askander199h@outlook.com>
2021-09-02 16:46:01 +0200

    Done

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,2 @@
 -- lists all bands
-SELECT DISTINCT `band_name`,
-                IFNULL(`split`, 2021) - `formed` as `lifespan`
-  FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
-  ORDER BY `lifespan` DESC;
+SELECT DISTINCT band_name, (IFNULL(`split`, 2021) - formed) AS lifespan FROM metal_bands WHERE style LIKE 'Glam rock' ORDER BY lifespan DESC;
